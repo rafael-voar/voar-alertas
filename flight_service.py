87368274faa2b11428ab756adbd5b330d5f3d5e6
rafael-voar <rafacrazy149@gmail.com>
2026-05-23 12:20:55 -0400

    fix sortBy price_low

--- flight_service.py
+++ flight_service.py
@@ -86,7 +86,7 @@ def buscar_voos_skyscanner(demanda):
         'date': demanda.data_ida,
         'cabinClass': 'economy',
         'adults': str(demanda.adultos),
-        'sortBy': 'cheapest',
+        'sortBy': 'price_low',
         'currency': demanda.moeda,
         'market': 'BR',
         'countryCode': 'BR',
